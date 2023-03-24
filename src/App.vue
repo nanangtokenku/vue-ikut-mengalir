c830b1ef99f8e6f26be2e82f37902d681fc3a5d2
nanangtokenku <81137154+nanangtokenku@users.noreply.github.com>
2023-03-25 06:04:41 +0800

    2 files modified

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="https://vuejs.org/images/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js ViApp"/>
+    <HelloWorld msg="Welcome to"/>
+    <HelloWorld msgDetail="ViApp"/>
   </div>
 </template>
 
