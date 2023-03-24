ab5341e10dfe8298b09b5274a58901bca03d62e6
nanangtokenku <81137154+nanangtokenku@users.noreply.github.com>
2023-03-25 06:48:11 +0800

    image width

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="https://vuejs.org/images/logo.png" />
+    <img alt="Vue logo" src="https://vuejs.org/images/logo.png" width="100" />
     <HelloWorld msg="Welcome To" />
     <HelloWorld msgDetail="ViApp" />
     <ViButton judul="PT. AAA" />
